2d080b6870623a90d5195b40e1c0a5444523b572
shahsajib95 <sajib.shahariar97@gmail.com>
2021-11-13 09:48:29 +0600

    cors

--- server/index.ts
+++ server/index.ts
@@ -12,13 +12,16 @@ import routes from './routes/index'
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(cors({
+    origin: `*`,
+    credentials: true
+  }))
 app.use(morgan('dev'))
 app.use(fileUpload()); 
 app.use(cookieParser())
 
 app.get("/", (req: Request, res: Response ) => {
-  res.setHeader("Access-Control-Allow-Origin", `${process.env.BASE_URL}`)
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
