292891bb03f8bc3632796393a56404e1e0af114d
shahsajib95 <sajib.shahariar97@gmail.com>
2021-11-13 18:43:19 +0600

    add

--- server/index.ts
+++ server/index.ts
@@ -29,10 +29,18 @@ const corsOpts = {
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors(corsOpts))
+// app.use(cors(corsOpts))
 app.use(morgan('dev'))
 app.use(fileUpload()); 
 app.use(cookieParser())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use('/api', routes)
 
