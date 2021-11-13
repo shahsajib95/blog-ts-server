167ed7dec3c7ee8846d81c04549a3339e1f66f2d
shahsajib95 <sajib.shahariar97@gmail.com>
2021-11-13 19:04:15 +0600

    add

--- server/index.ts
+++ server/index.ts
@@ -8,39 +8,19 @@ import morgan from 'morgan'
 import fileUpload from 'express-fileupload'
 import routes from './routes/index'
 
-const corsOpts = {
-  origin: '*',
-
-  methods: [
-    'GET',
-    'POST',
-    'DELETE',
-    'PATCH',
-    'UPDATE'
-  ],
-
-  allowedHeaders: [
-    'Content-Type',
-  ],
-};
 
 
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-// app.use(cors(corsOpts))
+app.use(cors({
+  origin: `${process.env.BASE_URL}`,
+  credentials: true
+}))
 app.use(morgan('dev'))
 app.use(fileUpload()); 
 app.use(cookieParser())
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 app.use('/api', routes)
 
