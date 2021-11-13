d6b3cdaf76fdab4872ff2b4fffedb4da5f5ab6db
shahsajib95 <sajib.shahariar97@gmail.com>
2021-11-13 18:18:49 +0600

    add

--- server/index.ts
+++ server/index.ts
@@ -8,10 +8,28 @@ import morgan from 'morgan'
 import fileUpload from 'express-fileupload'
 import routes from './routes/index'
 
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+    'DELETE',
+    'PATCH',
+    'UPDATE'
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(cors(corsOpts))
 app.use(morgan('dev'))
 app.use(fileUpload()); 
 app.use(cookieParser())
