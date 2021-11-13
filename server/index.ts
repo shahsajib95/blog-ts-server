a26bad30937d57898ed4ad6834735adc13066b9d
shahsajib95 <sajib.shahariar97@gmail.com>
2021-11-13 18:09:28 +0600

    add

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import { Request, Response } from "express";
 
 import express from 'express'
 import cors from 'cors'
@@ -12,23 +11,10 @@ import routes from './routes/index'
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors({
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-  }))
+app.use(cors())
 app.use(morgan('dev'))
 app.use(fileUpload()); 
 app.use(cookieParser())
-
-app.get("/", (req: Request, res: Response ) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-   });
 
 app.use('/api', routes)
 
