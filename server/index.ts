97eebb448194048b6432c1e80651ef3feb3544dd
shahsajib95 <sajib.shahariar97@gmail.com>
2021-11-13 09:42:56 +0600

    cors

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import { Request, Response } from "express";
 
 import express from 'express'
 import cors from 'cors'
@@ -18,6 +19,14 @@ app.use(cors({
 app.use(morgan('dev'))
 app.use(fileUpload()); 
 app.use(cookieParser())
+
+app.get("/", (req: Request, res: Response ) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+   });
 
 app.use('/api', routes)
 
